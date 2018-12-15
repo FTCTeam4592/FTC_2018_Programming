e2232b4a4a0c2c5c8413e4301b449fc030f17f83
Jai Kumar <mindtroopers10040@gmail.com>
2018-12-15 16:52:08 -0500

    added gyro testing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/2018-19 CODE/Auto_Testing/CraterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/2018-19 CODE/Auto_Testing/CraterAuto.java
@@ -1,14 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
-
 
 
 @Autonomous(name="AUTONOMOUS", group="Encoder")
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/2018-19 CODE/Auto_Testing/GyroTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name="GyroTest")
+public class GyroTest extends RobotBase {
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
